23101a33150d3d17867ed4247d55422860275c27
arckhy <47147787+arckhy@users.noreply.github.com>
2024-09-03 06:53:49 +0700

    update be appsetting to env var

--- TestCode_BE/Program.cs
+++ TestCode_BE/Program.cs
@@ -4,15 +4,25 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddEnvironmentVariables();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
+string dbServer = Environment.GetEnvironmentVariable("DB_SERVER");
+string dbDatabase = Environment.GetEnvironmentVariable("DB_DATABASE");
+
+
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer($"Server={dbServer};Database={dbDatabase};Trusted_Connection=True;TrustServerCertificate=True;"));
+
+
+builder.Services.AddControllers();
+//builder.Services.AddDbContext<DataContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
